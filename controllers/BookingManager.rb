4997b449f7448d77e9e6e43871f4ed2ce05d8f47
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-07 00:28:33 +0300

    Outline the structure (what methods are present) of the manager class.

---
+++ controllers/BookingManager.rb
@@ -0,0 +1,23 @@
+class BookingManager
+  def initialize(console, booking_model, resource_model,user_model)
+    @console = console
+    @booking_model = booking_model
+    @resource_model = resource_model
+    @user_model = user_model
+  end
+
+  def list_users
+  end
+  def list_resources
+  end
+  def list_bookings
+  end
+  def create_user
+  end
+  def add_resource
+  end
+  def make_booking
+  end
+  def cancel_booking
+  end
+end
