7b9160f4d266bac3541dcc5a60c89a096d04cd79
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-07 01:40:40 +0300

    Create a query that is executed everytime resources  class is created.

--- models/resource.rb
+++ models/resource.rb
@@ -1,9 +1,34 @@
 class Resource
-  # Data class for representing each individual resource
-  attr_accessor :id , :name, :category
-  def intialize(id, name , category)
-    @id = id
-    @name = name
-    @category = category
-  end
+  db = SQLite3::Database.new "booking_system.db"
+  #======== create users table if it doesn't exist
+  db.execute <<~SQL
+  CREATE TABLE IF NOT EXISTS users (
+      id TEXT PRIMARY KEY,
+      name TEXT NOT NULL,
+      role TEXT NOT NULL
+  );
+  SQL
+  #======== create resources table if it doesn't exist
+  db.execute <<~SQL
+  CREATE TABLE IF NOT EXISTS resources (
+      id TEXT  PRIMARY KEY,
+      name TEXT NOT NULL,
+      category TEXT NOT NULL
+  );
+  SQL
+  #======= create bookings table if it doesn't exist
+  db.execute <<~SQL
+  CREATE TABLE IF NOT EXISTS bookings (
+      id TEXT PRMIARY KEY,
+      user_id TEXT NOT NULL,
+      resource_id TEXT NOT NULL,
+      start_date TEXT NOT NULL,
+      end_date TEXT NOT NULL,
+      status TEXT NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users (id),
+      FOREIGN KEY (resource_id) REFERENCES resources (id)
+  );
+  SQL
+  # =============================================
+
 end
