07c7ec998fbb9418582c17bd8527994536158506
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-07 14:45:32 +0300

    Define how create_user method of booking_manager works

--- controllers/BookingManager.rb
+++ controllers/BookingManager.rb
@@ -23,7 +23,8 @@ class BookingManager
     bookings = @booking_model.get_all_bookings
     return Terminal::Table.new title:bookings[:title], headings: bookings[:headings], rows: bookings[:rows]
   end
-  def create_user
+  def create_user name:, role:
+    @user_model.create_user name: , role:
   end
   def add_resource
   end
