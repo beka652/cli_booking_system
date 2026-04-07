f024f8efeef2cc84acf4b4053e95027364184daa
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-07 14:14:29 +0300

    Modify resource so that it has a title.
     Remove sql query to create booking and users table.
    
    create a method called get_all_resources that returns the resources table formatted as a hash table

--- models/resource.rb
+++ models/resource.rb
@@ -1,13 +1,8 @@
+require "sqlite3"
+
 class Resource
+  @title = "Resources"
   @db ||= SQLite3::Database.new "booking_system.db"
-  #======== create users table if it doesn't exist
-  @db.execute <<~SQL
-  CREATE TABLE IF NOT EXISTS users (
-      id TEXT PRIMARY KEY,
-      name TEXT NOT NULL,
-      role TEXT NOT NULL
-  );
-  SQL
   #======== create resources table if it doesn't exist
   @db.execute <<~SQL
   CREATE TABLE IF NOT EXISTS resources (
@@ -16,19 +11,16 @@ class Resource
       category TEXT NOT NULL
   );
   SQL
-  #======= create bookings table if it doesn't exist
-  @db.execute <<~SQL
-  CREATE TABLE IF NOT EXISTS bookings (
-      id TEXT PRMIARY KEY,
-      user_id TEXT NOT NULL,
-      resource_id TEXT NOT NULL,
-      start_date TEXT NOT NULL,
-      end_date TEXT NOT NULL,
-      status TEXT NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users (id),
-      FOREIGN KEY (resource_id) REFERENCES resources (id)
-  );
-  SQL
   # =============================================
 
+
+  def self.get_all_resources
+    result = @db.execute2 ("SELECT * FROM resources")
+
+    hash_result = {
+      title: @title,
+      headings: result.delete_at(0),
+      rows: result
+    }
+  end
 end
