5f11f4fd4eff48c42601b154331e03e5e1dfc447
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-07 14:04:47 +0300

    Remove the two sql queries for creating the resources and bookings table. Implelment get_all_users method of users to fetch data from the database and return the result formatted as hash.

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,7 @@
+require "sqlite3"
+
 class User
+  @title = "Users"
   @db ||= SQLite3::Database.new "booking_system.db"
   #======== create users table if it doesn't exist
   @db.execute <<~SQL
@@ -8,34 +11,15 @@ class User
       role TEXT NOT NULL
   );
   SQL
-  #======== create resources table if it doesn't exist
-  @db.execute <<~SQL
-  CREATE TABLE IF NOT EXISTS resources (
-      id TEXT  PRIMARY KEY,
-      name TEXT NOT NULL,
-      category TEXT NOT NULL
-  );
-  SQL
   #======= create bookings table if it doesn't exist
-  @db.execute <<~SQL
-  CREATE TABLE IF NOT EXISTS bookings (
-      id TEXT PRMIARY KEY,
-      user_id TEXT NOT NULL,
-      resource_id TEXT NOT NULL,
-      start_date TEXT NOT NULL,
-      end_date TEXT NOT NULL,
-      status TEXT NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users (id),
-      FOREIGN KEY (resource_id) REFERENCES resources (id)
-  );
-  SQL
-  # =============================================
 
   def self.get_all_users
-    @db.execute "SELECT * FROM users"
+    result = @db.execute2 "SELECT * FROM users"
     # continue tmrw from  here
+    hash_result = {
+      title: @title,
+      headings: result.delete_at(0),
+      rows:result
+    }
   end
-
-
-
 end
